a2fd44762d6a9f443ea747fb89f6dd2ca1d64cf4
jessican1212 <115198772+jessican1212@users.noreply.github.com>
2022-12-02 22:32:20 -0800

    homepage update: meal list works hopefully

--- js/home.js
+++ js/home.js
@@ -11,25 +11,31 @@ recipelink1 = document.getElementById("recipelink1");
 likebutton = document.getElementsByClassName("btn")[0];
 likebutton1 = document.getElementsByClassName("btn1")[0];
 cover = document.getElementById("cover");
-list = document.getElementById("list");
+meallist = document.getElementById("list");
 
 
 submitButton.addEventListener("click", function () {
+  likebutton.style.backgroundColor = '#EFECE4';
+  likebutton1.style.backgroundColor = '#EFECE4';
   if (likes.value != "") {
     setRecipe(likes.value);
   }
   setTimeout(() => {cover.style.opacity = "0.0"; }, 500);
 });
 
-likebutton.addEventListener("click", function onClick(event) {
+likebutton.addEventListener("click", function () {
   likebutton.style.backgroundColor = 'rgb(255, 202, 243)';
+  const meal = document.createElement('li')
+  meal.innerHTML = recipelink.innerText;
+  meallist.appendChild(meal);
 });
 
-likebutton1.addEventListener("click", function onClick(event) {
+likebutton1.addEventListener("click", function () {
   likebutton1.style.backgroundColor = 'rgb(255, 202, 243)';
+  const meal = document.createElement('li')
+  meal.innerHTML = recipelink1.innerText;
+  meallist.appendChild(meal);
 });
-
-
 
 async function setRecipe(ingredients) {
     let recipeData = await getRecipe(ingredients)
