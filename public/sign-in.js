2d8b86259cb89416ba068fce9e8dba40686fd8fd
April Zhang <april.zhang@berkeley.edu>
2022-12-07 19:00:45 -0800

    about page

--- public/sign-in.js
+++ public/sign-in.js
@@ -1,16 +1,16 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js';
-import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, onAuthStateChanged} from 'https://www.gstatic.com/firebasejs/7.16.1/firebase-auth.js';
-//import { getFirestore, addDoc, collection, query, where, getDocs, Timestamp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB74ZoL9d5AbRNQ_b2QBkR78_RfsuOsgX0",
-    authDomain: "meal-buddy2.firebaseapp.com",
-    projectId: "meal-buddy2",
-    storageBucket: "meal-buddy2.appspot.com",
-    messagingSenderId: "120395499979",
-    appId: "1:120395499979:web:6c0127a395d6a726044e4e"
-  };
+  apiKey: "AIzaSyB74ZoL9d5AbRNQ_b2QBkR78_RfsuOsgX0",
+  authDomain: "meal-buddy2.firebaseapp.com",
+  databaseURL: "https://meal-buddy2-default-rtdb.firebaseio.com",
+  projectId: "meal-buddy2",
+  storageBucket: "meal-buddy2.appspot.com",
+  messagingSenderId: "120395499979",
+  appId: "1:120395499979:web:6c0127a395d6a726044e4e"
+};
   
 
 const app = initializeApp(firebaseConfig);
@@ -19,33 +19,24 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-signInBtn.onclick = () => {
-  signInWithPopup(auth, provider);
-}
-
-/*
-onAuthStateChanged(auth, (user) => {
-  // user argument is null if a user signs out.
-  if (user) {
-      // If a user signs in:
-      whenSignedIn.hidden = false;
-      whenSignedOut.hidden = true;
-      userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
-  } else {
-      //////// QUESTION 2: What happens when a user signs out? (The previous lines are a hint!) ////////
-      whenSignedIn.hidden = true;
-      whenSignedOut.hidden = false
-      ////////
-  }
-  
-});
-*/
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 
-//signInBtn.onclick = () => signInWithPopup(auth, provider);
-
-//const db = getFirestore(app);
-//let likedRecipes = collection(db, "liked_recipes")
-
-
-
+//signInBtn.onclick = () => {signInWithPopup(auth, provider);}
